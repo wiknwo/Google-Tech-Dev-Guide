28381263b10ed6fbcc4e2cceb9aa765b811308ad
wiknwo <spinningthewww@outlook.com>
2021-01-19 10:39:17 +0000

    Mostly correct but not returning correct value for one test case [2, 3, 4, 1, 2]

---
+++ canbalance.py
@@ -0,0 +1,15 @@
+# William Ikenna-Nwosu (wiknwo)
+# 
+# canBalance
+# 
+# Given a non-empty array, return true if there is a place 
+# to split the array so that the sum of the numbers on one 
+# side is equal to the sum of the numbers on the other 
+# side.
+#
+# canBalance([1, 1, 1, 2, 1]) → true
+# canBalance([2, 1, 1, 2, 1]) → false
+# canBalance([10, 10]) → true
+def canbalance(numbers):
+    totalsum = sum(numbers)
+    return totalsum % 2 == 0
